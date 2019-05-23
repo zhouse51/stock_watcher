c3d625a1d779afa57af729b03e9a08163fdf064d
James Zhou <james.zhou@apervita.com>
2019-05-23 15:19:01 -0500

    btc watch

--- apps/coinch.py
+++ apps/coinch.py
@@ -4,31 +4,20 @@ from utils.Config import Config
 import calculator.Statistics as statistics
 
 # TODO load from data store
-stocks = {
-    # 'NDAQ': None,
-    'ZM': {
+btc = {
+     'BTCUSD': {
         'date': '2019-05-22',
-        'price': 84.88,
+        'price': 90.06,
+        'unit_price': 7827.67,
         'type': 'buy',
-        'shares': 11
-    },
-    # 'PINS': {
-    #     'price': 26.48,
-    #     'type': 'buy',
-    #     'shares': 15
-    # },
-    # 'BYND': None,
-    # 'LK': None
+        'shares': 0.01150479
     }
+}
 
 
 def process_data():
-    stock_quotes = rbh.quotes_data([symbol for symbol in stocks])
-    fundamentals = {
-        symbol: rbh.get_fundamentals(symbol) for symbol in stocks
-    }
-
-    statistics.stocks_details_output(stocks, stock_quotes, fundamentals, interval, checkpoint_rates, period_samples)
+    btc_quote = rbh.get_crypto_quotes(symbols=['BTCUSD']).get('results')
+    statistics.BTC_details_output(btc.get('BTCUSD'), btc_quote[0], interval, checkpoint_rates, period_samples)
     # statistics.store_data(stock_quotes)
 
 
@@ -37,8 +26,8 @@ rbh = Robinhood()
 
 interval = config.get_config('fetch_interval')
 
-checkpoint_rates = statistics.build_checkpoints(stocks)
-period_samples = statistics.init_period_sample_sequence(stocks)
+checkpoint_rates = statistics.build_checkpoints(btc)
+period_samples = statistics.init_period_sample_sequence(btc)
 
 if not rbh.login(username=config.get_config('robinhood.username'), password=config.get_config('robinhood.password')):
     print('Login failed.')
--- calculator/Statistics.py
+++ calculator/Statistics.py
@@ -21,17 +21,17 @@ def init_period_sample_sequence(stocks, interval=5, period=10):
     return samples_queue
 
 
-def build_checkpoints(stocks):
+def build_checkpoints(investments):
     checkpoint_rates = [round(1 + r/100, 2) for r in list(range(-20, 21))]
-    for stock in stocks:
-        transaction = stocks.get(stock)
+    for stock in investments:
+        transaction = investments.get(stock)
         if transaction:
             trans_price = transaction.get('price')
-            stocks.get(stock)['checkpoint_prices'] = [round(trans_price * checkpoint_rate, 2) for checkpoint_rate in checkpoint_rates]
+            investments.get(stock)['checkpoint_prices'] = [round(trans_price * checkpoint_rate, 2) for checkpoint_rate in checkpoint_rates]
     return checkpoint_rates
 
 
-def calculate_slop(stock_period_samples, interval=5, period=10):
+def calculate_slop(stock_period_samples, interval=5, period=10, ask_price_key='ask_price', bid_price_key='bid_price', market_price_key='last_trade_price'):
     number_of_samples = int((60 / interval) * period)
 
     ask_price_slope = 0
@@ -41,13 +41,13 @@ def calculate_slop(stock_period_samples, interval=5, period=10):
     x_value = list(range(0, len(stock_period_samples.get_items()[-1 * number_of_samples:])))
 
     if len(x_value) > 1:
-        ask_price_slope = (linregress(x_value, [float(q.get('ask_price')) for q in stock_period_samples.get_items()[-number_of_samples:]])).slope
-        bid_price_slope = (linregress(x_value, [float(q.get('bid_price')) for q in stock_period_samples.get_items()[-number_of_samples:]])).slope
-        last_trade_price_slope = (linregress(x_value, [float(q.get('last_trade_price')) for q in stock_period_samples.get_items()[-number_of_samples:]])).slope
+        ask_price_slope = (linregress(x_value, [float(q.get(ask_price_key)) for q in stock_period_samples.get_items()[-number_of_samples:]])).slope
+        bid_price_slope = (linregress(x_value, [float(q.get(bid_price_key)) for q in stock_period_samples.get_items()[-number_of_samples:]])).slope
+        last_trade_price_slope = (linregress(x_value, [float(q.get(market_price_key)) for q in stock_period_samples.get_items()[-number_of_samples:]])).slope
     return ask_price_slope, bid_price_slope, last_trade_price_slope
 
 
-def details_output(stocks, stock_quotes, fundamentals, interval, checkpoint_rates, period_samples):
+def stocks_details_output(stocks, stock_quotes, fundamentals, interval, checkpoint_rates, period_samples):
     for quote in stock_quotes:
         updated_at = util.uct_to_ny_time(quote.get('updated_at'))
         symbol = quote.get('symbol')
@@ -122,6 +122,58 @@ def details_output(stocks, stock_quotes, fundamentals, interval, checkpoint_rate
               )
 
 
+def BTC_details_output(btc_transaction, btc_quote, interval, checkpoint_rates, period_samples):
+    updated_at = datetime.datetime.strftime(datetime.datetime.now(), '%H:%M:%S')
+    ask_price = round(float(btc_quote.get('ask_price')), 3)
+    bid_price = round(float(btc_quote.get('bid_price')), 3)
+    mark_price = round(float(btc_quote.get('mark_price')), 3)
+
+    # calculated data
+    period_samples.get('BTCUSD').push(btc_quote)
+
+    ask_price_slope_5min, bid_price_slope_5min, mark_price_slope_5min = calculate_slop(period_samples.get('BTCUSD'), interval=interval, period=5, market_price_key='mark_price')
+    ask_price_slope_10min, bid_price_slope_10min, mark_price_slope_10min = calculate_slop(period_samples.get('BTCUSD'), interval=interval, period=10, market_price_key='mark_price')
+
+    ask_price_slope_output_5min = util.get_slop_output(ask_price_slope_5min)
+    bid_price_slope_output_5min = util.get_slop_output(bid_price_slope_5min)
+    mark_price_slope_output_5min = util.get_slop_output(mark_price_slope_5min)
+
+    ask_price_slope_output_10min = util.get_slop_output(ask_price_slope_10min)
+    bid_price_slope_output_10min = util.get_slop_output(bid_price_slope_10min)
+    mark_price_slope_output_10min = util.get_slop_output(mark_price_slope_10min)
+
+    bid_trade_diff = round(mark_price - bid_price, 3)
+    bid_trade_diff_rate = round(((mark_price - bid_price) / mark_price) * 100, 2)
+    bid_trade_diff_output = util.get_diff_output(bid_trade_diff)
+    bid_trade_diff_rate_output = util.get_diff_output(bid_trade_diff_rate, format='{:>5}', postfix='%')
+
+    trans_price = btc_transaction.get('price')
+    trans_type = btc_transaction.get('type')
+    trans_unit_price = btc_transaction.get('unit_price')
+    shares = btc_transaction.get('shares', 0)
+
+    total_value = mark_price * shares
+    trans_price_diff = round(total_value - trans_price, 2) if trans_type == 'buy' else 0
+    trans_price_diff_rate = round((trans_price_diff / trans_price) * 100, 2)
+
+    total_value_output = util.get_diff_output(round(total_value, 2), format='{:>6}', postfix='$')
+    total_price_diff_output = util.get_diff_output(trans_price_diff, format='{:>6}', postfix='$')
+    trans_price_diff_rate_output = util.get_diff_output(trans_price_diff_rate, format='{:>5}', postfix='%')
+
+    display = '[SELL] [' + trans_price_diff_rate_output + '] '
+    display += '[' + total_price_diff_output + '] [' + total_value_output + ']' if trans_type == 'buy' else ''
+
+    bid_trade_diff_rate, history_price_slope, trend_factor, trend = get_trend(btc_quote, period_samples.get('BTCUSD'), market_price_key='mark_price')
+
+    print(f'{updated_at:{5}} '
+          f'{ask_price:{9}} ({ask_price_slope_output_5min:{1}}{ask_price_slope_output_10min:{1}}) '
+          f'{bid_price:{9}} ({bid_price_slope_output_5min:{1}}{bid_price_slope_output_10min:{1}}) '
+          f'{mark_price:{9}} ({mark_price_slope_output_5min:{1}}{mark_price_slope_output_10min:{1}}) '
+          + bid_trade_diff_output + ' [' + bid_trade_diff_rate_output + '] ' +
+          f' --> {display if display else ""} '
+          f'{round(history_price_slope, 3)}, {round(trend_factor, 2)}, {trend}'
+          )
+
 def store_data(stock_quotes):
     for stock_quote in stock_quotes:
         # raw data
@@ -140,17 +192,17 @@ def store_data(stock_quotes):
             the_file.close()
 
 
-def get_trend(quote, history_quote_queue, interval=5, period=3):
+def get_trend(quote, history_quote_queue, interval=5, period=3, ask_price_key='ask_price', bid_price_key='bid_price', market_price_key='last_trade_price'):
     number_of_samples = int((60 / interval) * period)
 
     # bid_trade_diff_rate range -0.02 ~ +0.02 (-3% ~ +3%)
-    quote_bid_price = float(quote.get('bid_price'))
-    quote_last_trade_price = float(quote.get('last_trade_price'))
+    quote_bid_price = float(quote.get(bid_price_key))
+    quote_last_trade_price = float(quote.get(market_price_key))
     bid_trade_diff_rate = ((quote_last_trade_price - quote_bid_price) / quote_last_trade_price)
 
     # slop range -0.004 ~ 0.004
     x_value = list(range(0, len(history_quote_queue.get_items()[-1 * number_of_samples:])))
-    y_value = [float(history_quote.get('last_trade_price')) for history_quote in history_quote_queue.get_items()[-number_of_samples:]]
+    y_value = [float(history_quote.get(market_price_key)) for history_quote in history_quote_queue.get_items()[-number_of_samples:]]
     history_price_slope = None
     if len(x_value) > 1:
         history_price_slope = (linregress(x_value, y_value)).slope
@@ -159,11 +211,12 @@ def get_trend(quote, history_quote_queue, interval=5, period=3):
     trend_factor = (bid_trade_diff_rate * 50) * 0.4 + (history_price_slope * 71) * 0.6
     # +: up, -: down
     # range 0 ~ +/-1, can be over
-    # 0 ~ 0.2: flat
-    # 0.2 ~ 0.5: slow
-    # >= 0.5: fast
+    # 0 ~ 0.2: slow: 1
+    # 0.2 ~ 0.5: mild: 2
+    # >= 0.5: fast: 3
+    # > 1.0: over: 4
     # TREND: -2, -1, 0, 1, 2
-    trend = 0 if abs(trend_factor) < 0.2 else (1 if abs(trend_factor) < 0.5 else 2)
+    trend = 0 if abs(trend_factor) == 0 else (1 if abs(trend_factor) < 0.2 else (2 if abs(trend_factor) < 0.5 else (3 if abs(trend_factor) < 1 else 4)))
     trend = (-1 * trend) if trend_factor < 0 else trend
 
     print('diff: ' + str(bid_trade_diff_rate) + ', slop: ' + str(history_price_slope) + ', factor: ' + str(trend_factor) + ', trend: ' + str(trend))
